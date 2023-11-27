e28db96c3d5f5938f9ae1ad937216e8f969279dd
Sergey Zubanov <serge-zubanov@yandex.ru>
2023-11-27 17:46:52 +0800

    lazy load

---
+++ hardsoftskills/LazyLoad/LazyLoad.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LazyLoad;
+
+class LazyLoad
+{
+    private ?int $price = null;
+
+    private PriceService $priceService;
+
+    public function __construct($productId, PriceService $priceService)
+    {
+        $this->productId = $productId;
+        $this->priceService = $priceService;
+    }
+
+    public function getPrice(): int
+    {
+        if ($this->price === null) {
+            // Если цена еще не была запрошена, запросить у PriceService и кэшировать
+            $this->price = $this->priceService->getPrice($this->productId);
+        }
+
+        return $this->price;
+    }
+}
+
+class PriceService {
+    public function getPrice($productId): int
+    {
+        return 100;
+    }
+}
+
+$lazyLoad = new LazyLoad(1, new PriceService());
+
+print_r($lazyLoad->getPrice().PHP_EOL);
+print_r($lazyLoad->getPrice().PHP_EOL);
